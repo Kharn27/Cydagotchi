853691423ba38d06cb85b66b6a607cf3188f7291
Kharn27 <32738307+Kharn27@users.noreply.github.com>
2025-11-21 14:39:37 +0100

    Normalize scenes and centralize button handling

--- EspGotchi.ino
+++ EspGotchi.ino
@@ -24,20 +24,42 @@ enum AppState {
   STATE_TITLE,
   STATE_MENU,
   STATE_NEW_PET,
-  STATE_LOAD_PET
+  STATE_GAME
 };
 
 AppState appState = STATE_TITLE;
 
-// --- Définition de boutons simples ---
+// --- Définition de boutons enrichis ---
+typedef void (*ButtonAction)();
+
 struct Button {
   int16_t x, y, w, h;
   const char* label;
+  uint16_t fillColor;
+  uint16_t textColor;
+  uint16_t borderColor;
+  ButtonAction onTap;
 };
 
-Button btnNewPet  = { 60, 110, 200, 40, "Créer un nouveau Cydagotchi" };
-Button btnLoadPet = { 60, 160, 200, 40, "Load Pet" };
+// Déclarations de callbacks de boutons
+void actionStartNewPet();
+void actionLoadPet();
+void actionStartGameFromNewPet();
 
+// --- Définition des boutons par scène ---
+Button menuButtons[] = {
+  { 60, 110, 200, 40, "Créer un nouveau Cydagotchi", TFT_DARKGREEN, TFT_WHITE, TFT_WHITE, actionStartNewPet },
+  { 60, 160, 200, 40, "Load Pet", TFT_DARKGREY, TFT_WHITE, TFT_WHITE, actionLoadPet }
+};
+
+Button newPetButtons[] = {
+  { 70, 170, 180, 40, "Démarrer le jeu", TFT_GREEN, TFT_WHITE, TFT_WHITE, actionStartGameFromNewPet }
+};
+
+const size_t MENU_BUTTON_COUNT   = sizeof(menuButtons) / sizeof(menuButtons[0]);
+const size_t NEWPET_BUTTON_COUNT = sizeof(newPetButtons) / sizeof(newPetButtons[0]);
+
+// --- Dessins d'écrans ---
 void drawTitleScreen() {
   tft.fillScreen(TFT_BLACK);
 
@@ -53,28 +75,39 @@ void drawTitleScreen() {
 }
 
 // Dessine un bouton rectangulaire avec un texte centré
-void drawButton(const Button& b, uint16_t colorFill, uint16_t colorText) {
-  tft.fillRoundRect(b.x, b.y, b.w, b.h, 8, colorFill);
-  tft.drawRoundRect(b.x, b.y, b.w, b.h, 8, TFT_WHITE);
+void drawButton(const Button& b) {
+  tft.fillRoundRect(b.x, b.y, b.w, b.h, 8, b.fillColor);
+  tft.drawRoundRect(b.x, b.y, b.w, b.h, 8, b.borderColor);
 
   tft.setTextDatum(MC_DATUM);
-  tft.setTextColor(colorText, colorFill);
+  tft.setTextColor(b.textColor, b.fillColor);
   tft.setTextFont(2);
   tft.drawString(b.label, b.x + b.w / 2, b.y + b.h / 2);
 }
 
-void drawMenuScreen() {
-  tft.fillScreen(TFT_NAVY);
+// --- Déclarations écrans ---
+void drawMenuScreen();
+void drawNewPetScreen();
+void drawGameScreen();
 
-  // Titre en haut
-  tft.setTextDatum(MC_DATUM);
-  tft.setTextColor(TFT_CYAN, TFT_NAVY);
-  tft.setTextFont(4);
-  tft.drawString("Main Menu", SCREEN_W / 2, 40);
+// --- Gestion centralisée des scènes ---
+void changeScene(AppState next) {
+  appState = next;
 
-  // Boutons
-  drawButton(btnNewPet,  TFT_DARKGREEN, TFT_WHITE);
-  drawButton(btnLoadPet, TFT_DARKGREY,  TFT_WHITE);
+  switch (appState) {
+    case STATE_TITLE:
+      drawTitleScreen();
+      break;
+    case STATE_MENU:
+      drawMenuScreen();
+      break;
+    case STATE_NEW_PET:
+      drawNewPetScreen();
+      break;
+    case STATE_GAME:
+      drawGameScreen();
+      break;
+  }
 }
 
 bool getTouch(int16_t &x, int16_t &y) {
@@ -105,6 +138,75 @@ bool isInside(const Button& b, int16_t x, int16_t y) {
           y >= b.y && y <= b.y + b.h);
 }
 
+// Gestion centralisée du tactile pour une liste de boutons
+bool processTouchForButtons(Button* buttons, size_t count) {
+  int16_t x, y;
+  if (!getTouch(x, y)) return false;
+
+  ButtonAction actionToFire = nullptr;
+
+  for (size_t i = 0; i < count; ++i) {
+    if (isInside(buttons[i], x, y)) {
+      actionToFire = buttons[i].onTap;
+      break;
+    }
+  }
+
+  // Debounce + attendre relâchement
+  delay(150);
+  while (ts.touched()) {}
+
+  if (actionToFire) {
+    actionToFire();
+    return true;
+  }
+
+  return false;
+}
+
+// --- Dessins d'écrans ---
+void drawMenuScreen() {
+  tft.fillScreen(TFT_NAVY);
+
+  // Titre en haut
+  tft.setTextDatum(MC_DATUM);
+  tft.setTextColor(TFT_CYAN, TFT_NAVY);
+  tft.setTextFont(4);
+  tft.drawString("Main Menu", SCREEN_W / 2, 40);
+
+  for (size_t i = 0; i < MENU_BUTTON_COUNT; ++i) {
+    drawButton(menuButtons[i]);
+  }
+}
+
+void drawNewPetScreen() {
+  tft.fillScreen(TFT_DARKGREY);
+
+  tft.setTextDatum(MC_DATUM);
+  tft.setTextColor(TFT_WHITE, TFT_DARKGREY);
+  tft.setTextFont(4);
+  tft.drawString("Nouveau Pet", SCREEN_W / 2, 40);
+
+  tft.setTextFont(2);
+  tft.drawString("Setup rapide placeholder", SCREEN_W / 2, 100);
+
+  for (size_t i = 0; i < NEWPET_BUTTON_COUNT; ++i) {
+    drawButton(newPetButtons[i]);
+  }
+}
+
+void drawGameScreen() {
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextDatum(MC_DATUM);
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.setTextFont(4);
+  tft.drawString("GAME", SCREEN_W / 2, SCREEN_H / 2 - 20);
+
+  tft.setTextFont(2);
+  tft.setTextColor(TFT_LIGHTGREY, TFT_BLACK);
+  tft.drawString("Boucle de jeu a venir", SCREEN_W / 2, SCREEN_H / 2 + 20);
+}
+
 
 void setup() {
   Serial.begin(115200);
@@ -118,60 +220,52 @@ void setup() {
   tft.init();
   tft.setRotation(1);
 
-  drawTitleScreen();
+  changeScene(STATE_TITLE);
 }
 
 void loop() {
-  static uint32_t lastTouchRelease = 0;
-  int16_t x, y;
-
   switch (appState) {
     case STATE_TITLE:
       // On quitte l’écran de titre sur premier tap
-      if (getTouch(x, y)) {
-        // petit debounce
-        delay(150);
-        while (ts.touched()) {} // attendre relâchement
-        appState = STATE_MENU;
-        drawMenuScreen();
+      {
+        int16_t x, y;
+        if (getTouch(x, y)) {
+          delay(150);
+          while (ts.touched()) {} // attendre relâchement
+          changeScene(STATE_MENU);
+        }
       }
       break;
 
     case STATE_MENU:
-      if (getTouch(x, y)) {
-        delay(150);
-        while (ts.touched()) {}
-
-        if (isInside(btnNewPet, x, y)) {
-          Serial.println("New Pet selected");
-          appState = STATE_NEW_PET;
-          tft.fillScreen(TFT_BLACK);
-          tft.setTextDatum(MC_DATUM);
-          tft.setTextColor(TFT_GREEN, TFT_BLACK);
-          tft.setTextFont(2);
-          tft.drawString("Creating a new pet...", SCREEN_W/2, SCREEN_H/2);
-          // TODO: ici on enchainera sur la creation de pet
-        }
-        else if (isInside(btnLoadPet, x, y)) {
-          Serial.println("Load Pet selected");
-          appState = STATE_LOAD_PET;
-          tft.fillScreen(TFT_BLACK);
-          tft.setTextDatum(MC_DATUM);
-          tft.setTextColor(TFT_YELLOW, TFT_BLACK);
-          tft.setTextFont(2);
-          tft.drawString("Loading pet...", SCREEN_W/2, SCREEN_H/2);
-          // TODO: plus tard : chargement SPIFFS
-        }
-      }
+      processTouchForButtons(menuButtons, MENU_BUTTON_COUNT);
       break;
 
     case STATE_NEW_PET:
       // pour l'instant on ne fait rien, on restera sur cet écran
       // plus tard : générer gènes + passer à l'écran "jeu"
+      processTouchForButtons(newPetButtons, NEWPET_BUTTON_COUNT);
       break;
 
-    case STATE_LOAD_PET:
-      // idem : plus tard -> lecture SPIFFS + écran "jeu"
+    case STATE_GAME:
+      // TODO: gérer les interactions du jeu principal
       break;
   }
+}
+
+// --- Actions de boutons ---
+void actionStartNewPet() {
+  Serial.println("New Pet selected");
+  changeScene(STATE_NEW_PET);
+}
+
+void actionLoadPet() {
+  Serial.println("Load Pet selected");
+  // En attendant le module de chargement, on entre en jeu directement
+  changeScene(STATE_GAME);
+}
+
+void actionStartGameFromNewPet() {
+  Serial.println("Starting game from NEW_PET");
+  changeScene(STATE_GAME);
 }
